7597205de0e4336b8b57770019de7654fa466234
alvinkgbateh <alvingbatehslgmail.com>
2022-10-13 19:33:05 -0700

    C program that prints exactly

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/##
+# Main - Entry
+# Return: Alway 0
+#/
+int main[void]
+{
+	puts("\"programming is like building a multi lingual puzzle");
+	return (0);
+}
--- 0x00-hello_world/READM.md
+++
@@ -1,1 +0,0 @@
-script that generates the assembly code of a C code and save it
