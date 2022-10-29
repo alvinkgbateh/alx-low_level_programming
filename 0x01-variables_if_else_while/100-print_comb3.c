fd6990805c623b732c5ba76a6cd1d2913acd5c51
Alvin G Kamara <91240003+alvinkgbateh@users.noreply.github.com>
2022-10-29 23:59:41 +0000

    updated 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,38 +1,32 @@
 #include <stdio.h>
+
 /**
- *  *main-program entry point.
- *   *Return:0 success, non zero fail
- *    */
+ *main - Prints combination of numbers
+ *
+ *Return: Always (Success)
+ *
+ */
+
 int main(void)
+
 {
-		int i, k = '0', j;
-
-			for (i = '0'; i <= '9'; i++)
-					{
-								for (j = k; j <= '9'; j++)
-											{
-															if (i != j)
-																			{
-																								putchar(i);
-																												putchar(j);
-																															}
-																		if (i == j)
-																						{
-																											continue;
-																														}
-																					if (i == '8' && j == '9')
-																									{
-																														break;
-																																	}
-																								else
-																												{
-																																	putchar(',');
-																																					putchar(' ');
-																																								}
-																										}
-										k++;
-											}
-				putchar('\n');
-					return (0);
+int c, i;
+for (c = '0'; c <= '9'; c++)
+{
+for (i = '0'; i <= '9'; i++)
+{
+if (c < i)
+{
+putchar(c);
+putchar(i);
+if (c != '8' || (c == '8' && i != '9'))
+{
+putchar(',');
+putchar(' ');
 }
-
+}
+}
+}
+putchar('\n');
+return (0);
+}
