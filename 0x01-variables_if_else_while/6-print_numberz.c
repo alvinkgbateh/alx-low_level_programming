abfe9d2056e42114c49ded5098e5a629a4442b60
Alvin G Kamara <91240003+alvinkgbateh@users.noreply.github.com>
2022-10-29 23:50:30 +0000

    updated 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+
 /**
- *  *main-program entry point.
- *   *Return:0 no error, non zero if error
- *    */
+ *main - Prints the numbers since 0 to 9
+ *
+ *Return: Always (Success)
+ */
+
 int main(void)
 {
-		int i;
-
-			for (i = '0'; i <= '9'; i++)
-						putchar(i);
-				putchar('\n');
-					return (0);
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
 }
-
+putchar('\n');
+return (0);
+}
